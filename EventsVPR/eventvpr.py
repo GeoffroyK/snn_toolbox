6ca2927a4b46017ecefb38ba5527d7997855dbb1
GeoffroyK <geoffroy.keime@gmail.com>
2024-04-26 15:27:48 +0200

    add dataset for event data (time & spike binnings) and main script

--- EventsVPR/eventvpr.py
+++ EventsVPR/eventvpr.py
@@ -5,6 +5,11 @@ from torch.utils.data import Dataset
 from spikingjelly.activation_based import neuron, learning, functional
 
 class EventVPR(nn.Module):
+    """_summary_
+
+    Args:
+        nn (_type_): _description_
+    """
     def __init__(self, n_in, n_out, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
@@ -18,23 +23,52 @@ class EventVPR(nn.Module):
     def forward(self, x):
         return self.net(x)
     
-    def spikeBinning():
-        pass
-
-    def timeBinning():
-        pass
-
 class eventDataset(Dataset):
-    def __init__(self) -> None:
+    """_summary_
+
+    Args:
+        Dataset (_type_): _description_
+    """
+    def __init__(self, eventsPath) -> None:
         super().__init__()
         self.tbins = []
         self.sbins = []
 
+        self.eventPath = eventsPath
+
+        
+
+    def buildTimeBins(self) -> None:
+        pass
+
+    def buildSpikeBins(self) -> None:
+        pass
+
+    def read_img_file(self):
+        img_list = []
+        timestamps = []
+        with open(self.eventPath + "images.txt") as file:
+            for item in file:
+                timestamps.append(float(item.split(' ')[0].strip()))
+                img_list.append(self.eventPath + item.split(' ')[1].strip())
+        assert len(img_list) == len(timestamps)
+        return img_list, timestamps
+    
     def __len__(self) -> int:
         return len(self.tins) + len(self.sbins)
 
     def __getitem__(self, index) -> any:
         return super().__getitem__(index)
+
+    def spikeBinning():
+        """_summary_
+        """
+        pass
+
+    def timeBinning():
+        """_summary_
+        """
+        pass
 
 if __name__=="__main__":
     net = EventVPR(n_in=1,n_out=2)
---
+++ EventsVPR/main.py
@@ -0,0 +1,7 @@
+import argparse
+
+def argParse():
+    pass
+
+if __name__=="__main__":
+    pass
